8df584887d32a29cdcae0bb9ae8bbc6c11b52660
Alma Freiesleben <alma.rosager@gmail.com>
2022-02-22 10:54:23 +0100

    INC2: added pseudo controller

--- INC2/Controller.java
+++ INC2/Controller.java
@@ -1,3 +1,36 @@
 public class Controller {
+
+    private boolean is_left_fixed;
     
+    public Controller(boolean is_left_fixed) {
+        this.is_left_fixed = is_left_fixed;
+    }
+
+    public boolean checkCollision() { return false; }
+
+    public void robotStep() {
+        
+    }
+
+    public void randomWalk() {
+        if (is_left_fixed) {
+            // Compute the joint to send to simulator
+            robot_step();
+
+            // Check for collision, if a collision has occured choose another action
+            while (collision_detection()) {
+                robot_step();
+            }
+            // Change chamber
+            is_left_fixed = false;        
+        } else {
+            robot_step();
+    
+            while (collision_detection()) {
+                robot_step();
+            }
+
+            is_left_fixed = true;
+        }
+    }
 }
