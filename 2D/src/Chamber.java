463fdaeb6a55acbc99fec3396c13f5d508936f4b
Alma Freiesleben <alma.rosager@gmail.com>
2022-04-03 09:02:22 +0200

    2D: Works for left chamber moving north and right chamber moving north

--- 2D/src/Chamber.java
+++ 2D/src/Chamber.java
@@ -1,18 +1,15 @@
 public class Chamber {
-    private int motorDir;
     private int joint;
     private int dummy1;
     private int dummy2;
 
-    public Chamber(int joint, int dummy1, int dummy2, int motorDir) {
+    public Chamber(int joint, int dummy1, int dummy2) {
         this.joint = joint;
         this.dummy1 = dummy1;
         this.dummy2 = dummy2;
-        this.motorDir = motorDir;
     }
 
     public int getJoint() { return joint; }
     public int getDummy1() { return dummy1; }
     public int getDummy2() { return dummy2; }
-    public int getMotorDir() {return motorDir; }
 }
--- 2D/src/Controller.java
+++ 2D/src/Controller.java
@@ -6,84 +6,107 @@ import java.util.*;
 public class Controller {
 
     private int clientID;
-    private double radius = 0.8;
     private remoteApi sim;
     private int floor;
     private Chamber leftChamber;
     private Chamber rightChamber;
+    private boolean isLeftFixed;
+    private double radius = 0.8;
     private double H = 3;
     private double W = 3;
-    private boolean isLeftFixed;
+    private float prevA = 0;
 
     // TEST VARIABLES REMOVE!!! TODO
     double test_x = 0;
     double test_y = 0;
     int cnt = 0;
-    float prevA = 0;
 
     public Controller(int clientID, remoteApi sim, int[] handles) {
         this.clientID = clientID;
         this.sim = sim;
         this.floor = handles[0];
-        this.leftChamber = new Chamber(handles[4], handles[1], handles[2], -1);
-        this.rightChamber = new Chamber(handles[6], handles[8], handles[9], 1);
+        this.leftChamber = new Chamber(handles[4], handles[1], handles[2]);
+        this.rightChamber = new Chamber(handles[6], handles[8], handles[9]);
         isLeftFixed = true;
     }
 
-    public void randomWalk() {
-        int prevA = 0;
-
+    /*public void randomWalk() {
         while (true) { //TODO: while !isCovered()
-            int A = new Random().nextInt(361);
+            int M = new Random().nextInt(361) - 180;
 
             if (isLeftFixed) {
-                step(rightChamber, leftChamber, A);
+                step(rightChamber, leftChamber, M);
             } else {
-                step(leftChamber, rightChamber, A);
+                step(leftChamber, rightChamber, M);
             }
+        }
+    }*/
 
-            prevA = A;
+    public void test() {
+        step(rightChamber, leftChamber, -20);
+        step(leftChamber, rightChamber, 40);
+        step(rightChamber, leftChamber, -60);
+    }
+
+    public void test2() {
+        fixChamberToFloor(leftChamber);
+        sleep(500);
+        for (int m = 0 ; m >= -180; m-=20) {
+            sim.simxSetJointTargetPosition(clientID, leftChamber.getJoint(), (float) Math.toRadians(m), sim.simx_opmode_blocking);
+            sleep(500);
         }
     }
 
-    public void test() {
-        step(rightChamber, leftChamber, 20);
-        step(leftChamber, rightChamber, -40);
+    public void test3() {
+        fixChamberToFloor(leftChamber);
+        sleep(500);
+        float M = (float) Math.toRadians(-20);
+        sim.simxSetJointTargetPosition(clientID, leftChamber.getJoint(), M, sim.simx_opmode_blocking);
+        sleep(500);
+        freeChamberFromFloor(leftChamber);
+        sleep(500);
+
+        fixChamberToFloor(rightChamber);
+        sleep(500);
+        float newM = (float) Math.toRadians(40);
+        sim.simxSetJointTargetPosition(clientID, rightChamber.getJoint(), newM, sim.simx_opmode_blocking);
+        sleep(500);
     }
 
     private void step(Chamber moving, Chamber fixed, int M) {
         fixChamberToFloor(fixed);
         sleep(500);
-        moveChamber(fixed, moving, M, prevA);
+        moveChamber(moving, fixed, M);
         sleep(500);
         freeChamberFromFloor(fixed);
         sleep(500);
     }
 
-    private void moveChamber(Chamber fixed, Chamber moving, float M, float A) {
-        boolean hasCollided = checkCollision(fixed, M, A);
+    private void moveChamber(Chamber moving, Chamber fixed, float M) {
+        float predictedA = predictA(fixed, M);
+        boolean hasCollided = checkCollision(fixed, predictedA);
 
         if (!hasCollided) {
-            var t = sim.simxSetJointTargetPosition(clientID, fixed.getJoint(), (float) Math.toRadians(fixed.getMotorDir() * M), sim.simx_opmode_blocking);
+            sim.simxSetJointTargetPosition(clientID, fixed.getJoint(), (float) Math.toRadians(M), sim.simx_opmode_blocking);
             sleep(500);
             FloatWA pos = getPositionOfHandle(moving.getJoint());
             sleep(500);
-            if (Math.abs(test_x - pos.getArray()[0]) > 0.001 || Math.abs(test_y - pos.getArray()[1]) > 0.001) {
+            isLeftFixed = !isLeftFixed;
+            prevA = M;
+
+            if (Math.abs(test_x - pos.getArray()[0]) > 0.01 || Math.abs(test_y - pos.getArray()[1]) > 0.01) {
                 System.out.println(cnt);
                 System.out.println("predicted x: " + test_x + " predicted y: " + test_y);
                 System.out.println("actual x: " + pos.getArray()[0] + " actual y: " + pos.getArray()[1]);
             }
             cnt++;
-            isLeftFixed = !isLeftFixed;
         }
     }
 
-    private boolean checkCollision(Chamber fixed, float M, float A) {
+    private boolean checkCollision(Chamber fixed, float predictedA) {
         var pos = getPositionOfHandle(fixed.getJoint());
         float fixedX = pos.getArray()[0];
         float fixedY = pos.getArray()[1];
-
-        float predictedA = predictA(M, A);
 
         double x = fixedX + radius * Math.cos(Math.toRadians(predictedA));
         double y = fixedY + radius * Math.sin(Math.toRadians(predictedA));
@@ -94,11 +117,12 @@ public class Controller {
         return Math.abs(x) > W/2 || Math.abs(y) > H/2;
     }
 
-    private float predictA(float M, float A) {
-        float predictedA = -A + M;
-        if (predictedA > 360) return A - 360;
-        if (predictedA < 0) return A + 360;
-        return predictedA;
+    private float predictA(Chamber fixed, float M) {
+        if (fixed.equals(leftChamber) && M < 0) return (prevA < 0) ? (M * -1) + prevA : (M * -1) - prevA;
+        //if (fixed.equals(leftChamber) && M >= 0)  TODO
+        //if (fixed.equals(rightChamber) && M < 0)  TODO
+        if (fixed.equals(rightChamber) && M >= 0) return (prevA < 0) ? 180 - (M + prevA) : 180 - (M - prevA);
+        return 0;
     }
 
     private void fixChamberToFloor(Chamber chamber) {
