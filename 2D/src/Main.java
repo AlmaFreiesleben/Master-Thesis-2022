cb30c7a90c22959eb36c642038695eadb6a2ab9b
Alma Freiesleben <alma.rosager@gmail.com>
2022-05-31 19:44:44 +0200

    2D: last testing of statistical significance using Mann Whitney U test

--- 2D/src/Main.java
+++ 2D/src/Main.java
@@ -3,16 +3,16 @@ public class Main
     public static void main(String[] args)
     {
         Simulator sim = new Simulator();
-        World world = new World(3, 6, 0.4);
+        World world = new World(6, 11, 0.4);
         Lappa lappa = new Lappa(sim, world);
-        WallBumpController c = new WallBumpController(lappa, world);
-        c.wallBumpRecordResult();
+        RandomWalkController c = new RandomWalkController(lappa, world);
+        c.randomWalkRecord95Result();
         c.writeToFiles();
 
         for (int i = 1; i < 10; i++) {
             world.preloadWorld();
             lappa.preloadAbsoluteMotorMovement();
-            c.wallBumpRecordResult();
+            c.randomWalkRecord95Result();
             c.writeToFiles();
         }
 
